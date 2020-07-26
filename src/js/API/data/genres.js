867d2efaefda68dd96c25cb1d3ef2bc5d52a51f2
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-07-27 00:07:53 +0400

    change files format

--- src/js/API/data/genres.js
+++
@@ -1,7 +0,0 @@
-export const GENRES = [
-    "undefined", "age", "alone", "amazing", "anger", "anniversary", "architecture", "art", 
-    "attitude", "beauty", "best", "birthday", "business", "car", "change", "communication", 
-    "computers", "cool", "courage", "dad", "dating", "death", "design", "diet", "dreams", 
-    "education", "environmental", "equality", "experience", "failure", "faith", "family", 
-    "famous", "fear", "finance", "fitness", "food", "forgiveness", "freedom",
-]
